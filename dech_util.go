de39fbf092d3ec399f30fb4163e0f8da90957f3c
Ruangdech Kurunat <ruangdech@gmail.com>
2022-05-11 17:49:25 +0700

    update

---
+++ dech_util.go
@@ -0,0 +1,30 @@
+package dechutil
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// Convert a slice or array of a specific type to array of interface{}
+func ConvertSliceToArrInf(input interface{}) []interface{} {
+	val := reflect.ValueOf(input)
+	// There is no need to check, we want to panic if it's not slice or array
+	inf := make([]interface{}, val.Len())
+	for i := 0; i < val.Len(); i++ {
+		inf[i] = val.Index(i).Interface()
+	}
+	return inf
+}
+
+func PrintArrInf(data []interface{}) {
+	for _, v := range data {
+		fmt.Printf("\n%#v\n", v)
+	}
+}
+
+func PrintData(data interface{}) {
+	cv := ConvertSliceToArrInf(data)
+	for _, v := range cv {
+		fmt.Printf("\n%#v\n", v)
+	}
+}
